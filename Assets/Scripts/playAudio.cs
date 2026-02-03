72e0e9bd80d8c343e4488047dec29a7f1456628e
Slin46 <slin46@pratt.edu>
2026-02-03 15:20:15 -0500

    Update playAudio.cs

--- Assets/Scripts/playAudio.cs
+++ Assets/Scripts/playAudio.cs
@@ -15,8 +15,8 @@ public class playAudio : MonoBehaviour
     {
         if (collider.tag == "Player")
         {
-            StartCoroutine(FadeIn(true));
             StopAllCoroutines();
+            StartCoroutine(FadeIn(true));
         }
     }
 
